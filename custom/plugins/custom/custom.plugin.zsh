d6b6b3bac16d1f3fe74f2b36ca1e5b4cabf46c01
Noah Harvey <nharvey7@gtri.gatech.edu>
2015-09-10 14:36:07 -0400

    added a custom git alias

--- custom/plugins/custom/custom.plugin.zsh
+++ custom/plugins/custom/custom.plugin.zsh
@@ -5,4 +5,20 @@ alias vimnrd="vim +NERDTree"
 alias copy="xclip -sel 'clipboard' -i"
 alias newterm="/home/noah/scripts/newterm"
 
+#custom git aliases
+function default()
+{
+	if [[ -z $1 ]]; then
+		echo $2
+	else
+		echo $1
+	fi
 
+}
+
+function githubClone()
+{
+	git clone -b $(default $2 master) https://github.com/$1.git
+}
+
+alias ghcl=githubClone
