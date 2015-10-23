b61d39b358d4bafa0d858b4402a475b77c51b1bf
Noah Harvey <noah.harvey247@gmail.com>
2015-10-23 09:36:39 -0400

    added gazebo plugin add ros aliases
     * made the sourcing of ros system files more modular

--- custom/plugins/ros/ros.plugin.zsh
+++ custom/plugins/ros/ros.plugin.zsh
@@ -1,10 +1,14 @@
 #source the catkin_ws paths (see the ros documentation for the order of the path
 #installation which matters)
-rosPaths=("/opt/ros/indigo/setup.zsh" "$HOME/src/SensingAndGrasping/catkin_ws/devel/setup.zsh")
+rosPaths=("/opt/ros/$ROS_DISTRO")
+
+for caws in $CATKIN_WS; do
+	rosPaths+="$caws/devel"
+done
 
 for p in $rosPaths; do
 	if [[ -e $p ]]; then
-		source $p
+		source "$p/setup.zsh"
 	else
 		echo "Cannot find ROS path: $p"
 	fi
@@ -15,6 +19,14 @@ alias rl="roslaunch"
 alias rt="rostopic"
 alias rte="rostopic echo"
 alias rtl="rostopic list"
+alias rtp="rostopic pub"
 alias rn="rosnode"
 alias rn="rosnode list"
 alias rcd="roscd"
+alias rcm="catkin_make"
+alias rb="rosbag"
+alias rbp="rosbag play"
+alias rbi="rosbag info"
+alias tfv="rosrun tf view_frames && mv frames.pdf /tmp/frames.pdf"
+alias tfm="rosrun tf tf_monitor"
+alias tfe="rosrun tf tf_echo"
